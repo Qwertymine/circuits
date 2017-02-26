bf9ee543c2258cb1f50490e75b228fc34fa1410f
qwertymine3 <qwertymine3@googlemail.com>
2017-02-26 23:22:16 +0000

    Move wire nodes to seperate file

---
+++ wire_node.lua
@@ -0,0 +1,69 @@
+local c = circuits
+
+local wire = {
+	description = "Wire",
+	drawtype = "nodebox",
+	node_box = {
+		type = "connected",
+		fixed = {-0.2,-0.2,-0.2,0.2,0.2,0.2},
+		connect_top = {-0.2,-0.2,-0.2,0.2,0.5,0.2},
+		connect_bottom = {-0.2,-0.5,-0.2,0.2,0.2,0.2},
+		connect_front = {-0.2,-0.2,-0.5,0.2,0.2,0.2},
+		connect_back = {-0.2,-0.2,-0.2,0.2,0.2,0.5},
+		connect_left = {-0.5,-0.2,-0.2,0.2,0.2,0.2},
+		connect_right = {-0.2,-0.2,-0.2,0.5,0.2,0.2},
+	},
+	selection_box = {
+		type = "connected",
+		fixed = {-0.3,-0.3,-0.3,0.3,0.3,0.3},
+		connect_top = {-0.3,-0.3,-0.3,0.3,0.5,0.3},
+		connect_bottom = {-0.3,-0.5,-0.3,0.3,0.3,0.3},
+		connect_front = {-0.3,-0.3,-0.5,0.3,0.3,0.3},
+		connect_back = {-0.3,-0.3,-0.3,0.3,0.3,0.5},
+		connect_left = {-0.5,-0.3,-0.3,0.3,0.3,0.3},
+		connect_right = {-0.3,-0.3,-0.3,0.5,0.3,0.3},
+	},
+	collision_box = {
+		type = "fixed",
+		fixed = {{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},},
+	},
+	tiles = {"default_mese_block.png"},
+	paramtype = "light",
+	sunlight_propagates = true,
+	is_ground_content = false,
+	walkable = true,
+	groups = {dig_immediate=3,circuit_wire=1,circuit_raw_wire=1},
+	connects_to = {"group:circuit_wire", "group:circuit_consumer", "group:circuit_power"},
+	--[[
+	on_rightclick = function(pos,node)
+		local flags = node.param2
+		pos = c.npos(pos,node)
+		minetest.chat_send_all(flags)
+		local network, powered = get_wire_network(pos)
+		for _,real in pairs(network) do
+			local dir = c.rot_relative_pos(pos, real)
+			minetest.chat_send_all("{ " .. dir.x ..  ","
+				.. dir.y .. "," .. dir.z .. "}")
+		end
+		minetest.chat_send_all(tostring(powered))
+	end,
+	--]]
+	--after_place_node = function(pos,placer,itemstack,pointed_thing)
+	--[[
+	--	Circuits properties definition area
+	--]]
+	circuits = {
+		connects = c.local_area,
+		connects_to = {"circuit_consumer", "circuit_wire", "circuit_power"},
+		store_connect = "param2",
+		powering = function(npos, rpos)
+			return c.is_on(npos)
+		end
+	},
+}
+
+c.register_on_off("circuits:wire",wire,{},
+{
+	tiles = {"default_mese_block.png^[colorize:#111:160"},
+})
+
